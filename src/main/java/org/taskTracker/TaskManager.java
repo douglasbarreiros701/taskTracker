9e5ae515ef2f6742e5ba0ee4587d7e5a3409ad7c
Douglas Barreiros <douglasb701@gmail.com>
2025-12-04 12:21:02 -0300

    Criando uma lista de Task e salvando a Task na lista

--- src/main/java/org/taskTracker/TaskManager.java
+++ src/main/java/org/taskTracker/TaskManager.java
@@ -1,14 +1,16 @@
 package org.taskTracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskManager {
 
-
-
-
+    List<Task> taskList = new ArrayList<>();
 
     public void createTask(String description){
 
         Task task = new Task(description);
+        taskList.add(task);
         System.out.println("Task criada ");
 
     }
